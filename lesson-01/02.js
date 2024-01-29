649bf091721fb1753d072a968c84a03ecc11d0cd
DianaKernas <dianka.di@mail.ru>
2024-01-29 22:19:59 +0300

    Math.random() number1%2==0

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
 // code
+let number1 = Math.floor(Math.random() * 100)
+    if (number1%2==0) {
+    console.log("Число " + number1 + " четное");
+} else {
+    console.log("Число " + number1 + " нечетное");
+}
