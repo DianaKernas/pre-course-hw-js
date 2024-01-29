ff819ff3deca3861f41c0d6376b31635abec045a
DianaKernas <dianka.di@mail.ru>
2024-01-30 01:07:03 +0300

    01/02.js even/odd numbers

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,7 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
-    if (number1%2==0) {
-    console.log("Число " + number1 + " четное");
+if (number1%2==0) {
+    console.log(number1 + ":Это четное число");
 } else {
-    console.log("Число " + number1 + " нечетное");
+    console.log(number1 + ":Это нечетное число");
 }
