390fbf206644a5f0c9e0ad91f098a3f38a48a5d7
DianaKernas <dianka.di@mail.ru>
2024-01-30 17:33:18 +0300

    05/02.js copying objects

---
+++ lesson-02/05.js
@@ -0,0 +1,17 @@
+//Дан объект passport. Создай его копию и поменяй
+//имя с "Petr" на "Ivan", и убедись в консоли, что
+//у объекта passport имя осталось "Petr", а у
+//твоего нового объекта "Ivan".
+
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+console.log(passport.name + passport.surname)
+
+let data = passport;
+data.name="Ivan";
+console.log(data.name + data.surname)
+
+//Для того, чтобы проверить правильность данного
+//задания должно быть выведено 2 console.log
