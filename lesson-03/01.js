1a7e0b55a14f6a1d8f05fa277eac74dcf6542ec8
DianaKernas <dianka.di@mail.ru>
2024-02-04 00:31:39 +0300

    01/03.js filter() || join()

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,13 @@
-// code
+//У нас есть массив с цветами. При его помощи нам нужно вернуть (return)
+//формулу флага Германии («черный-красный-желтый»). Для этого тебе понадобятся
+//следующие методы: filter() и join(), а заодно вспомнить, что это
+//такое ||- это пригодится для условия в методе filter()
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString(parameter) {
+    const test = parameter => parameter == "черный" || parameter == "красный" || parameter == "желтый" ;
+    return colors.filter(test).join('-');
+
+};
+
+console.log(createColorString(colors.length));
