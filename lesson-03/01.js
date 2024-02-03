050094ed080a35ec1cdcb2989a78a24ef209fa99
DianaKernas <dianka.di@mail.ru>
2024-02-04 00:37:45 +0300

    01/03.js filter() || join()

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -4,10 +4,10 @@
 //такое ||- это пригодится для условия в методе filter()
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
-function createColorString(parameter) {
-    const test = parameter => parameter === "черный" || parameter === "красный" || parameter === "желтый" ;
-    return colors.filter(test).join('-');
+function createColorString() {
+    const color = parameter => parameter === "черный" || parameter === "красный" || parameter === "желтый" ;
+    return colors.filter(color).join('-');
 
 }
 
-console.log(createColorString(colors.length));
+console.log(createColorString());
