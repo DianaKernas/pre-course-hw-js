10a2a7b1316183495a31c8dd119f2ec5fb8bb073
DianaKernas <dianka.di@mail.ru>
2024-02-06 18:11:57 +0300

    06/02.js deep copying distribution Spread={...n}

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,5 +1,5 @@
-//Давайте-ка нашего парня, чтобы он не зазнавался переселим
-//в ‘city’ 'Bobryisk' , а потом опять же убедимся в консоли,
+// Давайте-ка нашего парня, чтобы он не //зазнавался переселим
+// в ‘city’ 'Bobryisk' , а потом опять же убедимся в консоли,
 // что мы все правильно скопировали. (Т.е. необходимо создать
 // глубокую копию объекта passportWithAddress,
 // а затем 'LA' заменить на 'Bobryisk' в city).
@@ -11,4 +11,12 @@ let passportWithAddress = {
         country: "USA",
         city: "LA"
     }
-}
+};
+console.log(passportWithAddress.address.city);
+
+let infoAboutPerson = { ... passportWithAddress };
+infoAboutPerson.address.city="Bobryisk";
+console.log(infoAboutPerson.address.city);
+
+//Для того, чтобы проверить правильность данного
+//задания должно быть выведено 2 console.log
